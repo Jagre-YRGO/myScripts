59d209c61a125fa4b601f75dc05c0628e8bd452a
Erik Jagre <erik.jagre@educ.goteborg.se>
2022-09-08 13:41:17 +0200

    Exempel termostaten adderad

---
+++ termostaten.py
@@ -0,0 +1,10 @@
+#läs user-input
+nuTemp = float(input('Ange nuTemp: '))
+
+#Evaluera
+if nuTemp < 25:
+   print('slå på värme!')
+elif nuTemp > 24:
+   print('slå av värme!')
+
+print('***Slut på program***')
